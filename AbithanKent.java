9b33a47b21e5289a908f54f5be57d09c04e89cc7
ClarkkCC <Clark113008@gmail.com>
2025-04-22 14:41:55 -0400

    Added Hero Code

--- AbithanKent.java
+++ AbithanKent.java
@@ -8,12 +8,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class AbithanKent extends Actor
 {
-    /**
-     * Act - do whatever the AbithanKent wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atTop = true;
     public void act()
     {
-        // Add your action code here.
+       if(Greenfoot.mouseClicked(null))
+       {
+           atTop = !atTop;
+       }
+       if(atTop)
+       {
+           setLocation(100,100);
+       }
+       else
+       {
+           setLocation(100,300);
+       }
     }
 }
