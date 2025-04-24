f65b92a826bec9a91693a880ce8cfd72dcbd9902
AbithanP <abithan.mayo@gmail.com>
2025-04-24 14:00:26 -0400

    Finish game

--- Banana.java
+++ Banana.java
@@ -15,6 +15,31 @@ public class Banana extends Actor
     
     public void act()
     {
-        // Add your action code here.
+        move(-10);
+        
+        if (getX() <= 0)
+        {
+            resetBanana();
+        }
+        
+        if (isTouching(AbithanKent.class))
+        {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
     }
 }
